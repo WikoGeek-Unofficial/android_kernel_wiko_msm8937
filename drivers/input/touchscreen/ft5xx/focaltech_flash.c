e23e2b79889d155629b40c96359352277cb4efcc
wangyanhui <yanhui.wang@tinno.com>
2016-05-31 19:28:43 +0800

    <REQ><HHABM-1><MSM8937.LA.1.0: common driver  tp upgrade for p7201>

--- drivers/input/touchscreen/ft5xx/focaltech_flash.c
+++ drivers/input/touchscreen/ft5xx/focaltech_flash.c
@@ -134,6 +134,8 @@ static unsigned char CTPM_FW[] = {
 
 #ifdef CONFIG_PROJECT_P7701
 	#include "ft5436_fw_app_p7701.i"
+#elif CONFIG_PROJECT_P7201
+	#include "FT5346_BOEN_TL_app_p7201.i"
 #else
 	#include "FT_Upgrade_App.i"
 #endif
