4b901b2b4cf35ec12cd15e2b9e7f15a77c7dcb92
yangliang <liang.yang@tinno.com>
2016-06-06 11:00:03 +0800

    <REQ><HHABM-116><P7701_MSM8937_6.0: modify 6 to 9 for 256ms debounce time for judgement by mistake p7701-androidM>
    Change-Id: Ib8501ed659d18e8365c77dbb050e77e5c8f81d83

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -2091,7 +2091,7 @@ static int wcd_mbhc_initialise(struct wcd_mbhc *mbhc)
 	WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_L_DET_EN, 1);
 
 	/* Insertion debounce set to 96ms */
-	WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_INSREM_DBNC, 6);
+	WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_INSREM_DBNC, 9);//yangliang modify 6 to 9 for 256ms debounce time for judgement by mistake 20160606;
 	/* Button Debounce set to 16ms */
 	WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_DBNC, 2);
 
