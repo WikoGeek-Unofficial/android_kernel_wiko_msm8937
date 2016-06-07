4e89f7a82aa5ca28c96007aa7c9604b012fa86d3
alik.hou <alik.hou@tinno.com>
2016-06-07 16:25:00 +0800

    <REQ><HCADCNA-1><项目工程配置>
    Change-Id: I2a27f18442a1942c258a341ec1e125c36c8e58a4

--- drivers/input/touchscreen/ft5xx/focaltech_flash.c
+++ drivers/input/touchscreen/ft5xx/focaltech_flash.c
@@ -134,7 +134,7 @@ static unsigned char CTPM_FW[] = {
 
 #ifdef CONFIG_PROJECT_P7701
 	#include "ft5436_fw_app_p7701.i"
-#elif defined(CONFIG_PROJECT_P7201)
+#elif defined(CONFIG_PROJECT_P7201)||defined(CONFIG_PROJECT_P7203)
 	#include "FT5346_BOEN_TL_app_p7201.i"
 #else
 	#include "FT_Upgrade_App.i"
